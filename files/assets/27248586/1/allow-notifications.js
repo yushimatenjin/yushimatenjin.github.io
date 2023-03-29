91dbf09de97f40c8ba6107f46f4ffa17ed5d6908
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2023-03-29 11:01:50 +0900

    fix

--- files/assets/27248586/1/allow-notifications.js
+++ files/assets/27248586/1/allow-notifications.js
@@ -1,41 +1,18 @@
 var AllowNotifications = pc.createScript("allowNotifications");
 
-// initialize code called once per entity
 AllowNotifications.prototype.initialize = function () {
-  if (!window.matchMedia("(display-mode: standalone)").matches) {
-    this.entity.element.on(
-      "click",
-      () => {
-        this.notifyMe();
-      },
-      this
-    );
-  } else {
+  this.entity.element.on("click", this.notifyMe, this);
+};
+
+AllowNotifications.prototype.notifyMe = function () {
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
   }
 
-  // this.entity.element.on("click", () => {
-  //     alert("Element notification ons")
-  // })
-};
-AllowNotifications.prototype.notifyMe = function () {
-  // Let's check if the browser supports notifications
-  //   if (!("Notification" in window)) {
-  //     alert("This browser does not support desktop notification");
-  //   }
-
-  // Let's check whether notification permissions have already been granted
   if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
     new Notification(`You have ${balance} coins !`);
   }
-
-  // Otherwise, we need to ask the user for permission
-//   if (Notification.permission !== "denied") {
-    Notification.requestPermission(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        new Notification("Hi there!");
-      }
-    });
-//   }
+  Notification.requestPermission().then(() => {
+      new Notification(`You have ${balance} coins !`);
+  })
 };
