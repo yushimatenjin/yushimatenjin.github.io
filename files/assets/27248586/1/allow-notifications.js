621fdb58b2d4b166b1f798f7112c1e0eb91b411e
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2023-03-29 11:21:12 +0900

    fix

--- files/assets/27248586/1/allow-notifications.js
+++ files/assets/27248586/1/allow-notifications.js
@@ -1,40 +1,38 @@
 var AllowNotifications = pc.createScript("allowNotifications");
 
 AllowNotifications.prototype.initialize = function () {
-    this.entity.element.on("click", this.notifyMe, this);
+  this.entity.element.on("click", this.notifyMe, this);
 };
 
 function showNotification(title, options) {
-    if (Notification.permission === 'granted') {
-      const notification = new Notification(title, options);
-      notification.onclick = () => {
-        // console.log('Notification clicked');
-        alert("Notification clicked")
-      };
-    } else {
-      console.error('Notification permission is not granted');
-    }
+  if (Notification.permission === "granted") {
+    const notification = new Notification(title, options);
+    notification.onclick = () => {
+      // console.log('Notification clicked');
+      alert("Notification clicked");
+    };
+  } else {
+    console.error("Notification permission is not granted");
   }
-  
+}
 
 AllowNotifications.prototype.notifyMe = function () {
-    if (!("Notification" in window)) {
-        alert("This browser does not support desktop notification");
-    }
-
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  } else if (Notification.permission === "granted") {
+    showNotification(`You have ${balance} coins !`, {
+      body: "Click to play",
+      icon: "/logo.png",
+    });
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(() => {
+      if (Notification.permission === "granted") {
+        alert("プッシュ通知が許可されました。");
         showNotification(`You have ${balance} coins !`, {
             body: "Click to play",
-            icon: "/logo.png"
+            icon: "/logo.png",
         });
-
-
-    if (Notification.permission !== "denied") {
-        Notification.requestPermission().then(() => {
-            if(Notification.permission === "granted") {
-
-               alert("Granted");
-               showNotification(`You have ${balance} coins !`, {});
-            }
-        });
-    }
+      }
+    });
+  }
 };
