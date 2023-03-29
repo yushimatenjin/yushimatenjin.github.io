25e8312963d057e5ec43213ebe8e721e1ce3f620
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2023-03-29 11:08:50 +0900

    fix

--- files/assets/27248586/1/allow-notifications.js
+++ files/assets/27248586/1/allow-notifications.js
@@ -1,21 +1,23 @@
 var AllowNotifications = pc.createScript("allowNotifications");
 
 AllowNotifications.prototype.initialize = function () {
-  this.entity.element.on("click", this.notifyMe, this);
+    this.entity.element.on("click", this.notifyMe, this);
 };
 
 AllowNotifications.prototype.notifyMe = function () {
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
 
-  if (Notification.permission === "granted") {
-    new Notification(`You have ${balance} coins !`);
-  }
+    if (Notification.permission === "granted") {
+        new Notification(`You have ${balance} coins !`);
+    }
 
-  if (Notification.permission !== "denied") {
-    Notification.requestPermission().then(() => {
-      new Notification(`You have ${balance} coins !`);
-    });
-  }
+    if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(() => {
+            new Notification(`You have ${balance} coins !`, {
+                body: `You have ${balance} coins !`
+            });
+        });
+    }
 };
